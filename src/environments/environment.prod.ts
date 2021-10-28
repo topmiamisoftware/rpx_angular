d4e0f9946e21d3b35588d61f0a7f61cb1fc603c6
Franco Petitfour <franco.petitfour001@gmail.com>
2021-10-28 09:27:43 -0400

    updated netlify staging url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://spotbie.com/',
+  baseUrl: 'https://spotbie-staging.netlify.app/',
   versionCheckURL : 'https://spotbie.com/version.json',
   google_maps_apiKey: 'AIzaSyC4Su0B2cBzsSpAF-Kphq_78uR8b5eA4_Q',
   google_places_apiAkey: 'AIzaSyChSn9IE6Dp0Jv8TS013np1b4X1rCsQt_E',
