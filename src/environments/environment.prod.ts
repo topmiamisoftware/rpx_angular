ebdc8ea2cbe46ccbd2645dc75de3ea474e9b8d5f
Franco Petitfour <franco.petitfour001@gmail.com>
2021-10-30 21:58:03 -0400

    still dre

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   versionCheckURL : 'https://spotbie.com/version.json',
   google_maps_apiKey: 'AIzaSyC4Su0B2cBzsSpAF-Kphq_78uR8b5eA4_Q',
   google_places_apiAkey: 'AIzaSyChSn9IE6Dp0Jv8TS013np1b4X1rCsQt_E',
-  apiEndpoint: 'https://api.spotbie.com/',
+  apiEndpoint: 'https://api.spotbie.com/api/',
   qrCodeScanBaseUrl : 'https://spotbie.com/loyalty-points/scan',
   ngrok: null
 };
