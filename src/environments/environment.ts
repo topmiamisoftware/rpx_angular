f95da7259713cc001a066fd253098c477ed80536
Franco Petitfour <franco.petitfour001@gmail.com>
2022-01-04 17:26:20 -0500

    november rain

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-let ngrok = 'https://41f5-2600-1700-4804-d5f0-b8f9-605-371f-c4de.ngrok.io/'
+let ngrok = 'https://92fa-2600-1700-4804-d5f0-2a94-49f1-7969-6a84.ngrok.io/'
 let baseUrl = 'https://192.168.1.65:4200/'
 
 export const environment = {
