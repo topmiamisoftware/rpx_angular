122873ffdb5702182d05bf30a6f5a00fc28c017e
Franco Petitfour <franco.petitfour001@gmail.com>
2022-04-15 18:00:28 -0400

    Cahnged demo api end point

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
   versionCheckURL: baseUrl+'version.json',
   google_maps_apiKey: 'AIzaSyC4Su0B2cBzsSpAF-Kphq_78uR8b5eA4_Q',
   google_places_apiAkey: 'AIzaSyChSn9IE6Dp0Jv8TS013np1b4X1rCsQt_E',
-  apiEndpoint: 'https://api.spotbie.com/api/',
+  apiEndpoint: 'https://api-demo.spotbie.com/api/',
   qrCodeLoyaltyPointsScanBaseUrl: baseUrl+'loyalty-points',
   qrCodeRewardScanBaseUrl: baseUrl+'reward',
   ngrok: null,
