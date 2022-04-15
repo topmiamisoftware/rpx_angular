917aab8ccaed07cd3ac6046d7b40354acbffd7a4
Franco Petitfour <franco.petitfour001@gmail.com>
2022-04-15 10:56:41 -0400

    Changed .env production variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 let baseUrl = 'https://demo.spotbie.com/'
 
 export const environment = {
-  production: false,
-  staging: true,
+  production: true,
+  staging: false,
   baseUrl: baseUrl,
   versionCheckURL: baseUrl+'version.json',
   google_maps_apiKey: 'AIzaSyC4Su0B2cBzsSpAF-Kphq_78uR8b5eA4_Q',
@@ -11,7 +11,7 @@ export const environment = {
   qrCodeLoyaltyPointsScanBaseUrl: baseUrl+'loyalty-points',
   qrCodeRewardScanBaseUrl: baseUrl+'reward',
   ngrok: null,
-  fakeLocation: true,
+  fakeLocation: false,
   myLocX: 25.786286,
   myLocY: -80.186562,
   publishableStripeKey: 'pk_live_51JrUwnGFcsifY4UhqQVtkwnats9SfiUseYMsCBpoa7361hvxq4uWNZcxL2nZnhhrqtX5vLs9EUFACK3VR60svKyc00BSbooqh8'
