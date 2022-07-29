2232d0cae815f130c0c1a397e840ba63fab0fd5e
0456franco <franco.petitfour001@gmail.com>
2022-07-29 01:26:09 -0400

    Made some changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 const baseUrl = 'https://spotbie.com/'
 
 export const environment = {
-  production: true,
-  staging: false,
+  production: false,
+  staging: true,
   baseUrl,
   versionCheckURL: baseUrl+'version.json',
   google_maps_apiKey: 'AIzaSyC4Su0B2cBzsSpAF-Kphq_78uR8b5eA4_Q',
@@ -11,7 +11,7 @@ export const environment = {
   qrCodeLoyaltyPointsScanBaseUrl: baseUrl+'loyalty-points',
   qrCodeRewardScanBaseUrl: baseUrl+'reward',
   ngrok: null,
-  fakeLocation: false,
+  fakeLocation: true,
   myLocX: 25.786286,
   myLocY: -80.186562,
   publishableStripeKey: 'pk_live_51JrUwnGFcsifY4UhqQVtkwnats9SfiUseYMsCBpoa7361hvxq4uWNZcxL2nZnhhrqtX5vLs9EUFACK3VR60svKyc00BSbooqh8'
