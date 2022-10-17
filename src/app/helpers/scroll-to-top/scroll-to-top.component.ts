99f108768a44247edaeb55e651a5bc9918c1350f
0456franco <franco.petitfour001@gmail.com>
2022-10-16 22:18:50 -0400

    Loyalty Points Update

--- src/app/helpers/scroll-to-top/scroll-to-top.component.ts
+++ src/app/helpers/scroll-to-top/scroll-to-top.component.ts
@@ -1,7 +1,7 @@
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import * as $ from 'jquery'
 
-const MY_NEWS_LINK = "https://help.rescue.org/donate/ukraine-acq?ms=gs_ppc_fy22_ukraine_mmus_feb&initialms=gs_ppc_fy22_ukraine_mmus_feb&gclid=CjwKCAiAg6yRBhBNEiwAeVyL0OZ3fFQMEO9dXqlmjgq1bGD-Xa8uKKQm-H-NiJeC_ag1juzzAX26XRoColMQAvD_BwE"
+const MY_NEWS_LINK = 'https://help.rescue.org/donate/ukraine-acq?ms=gs_ppc_fy22_ukraine_mmus_feb&initialms=gs_ppc_fy22_ukraine_mmus_feb&gclid=CjwKCAiAg6yRBhBNEiwAeVyL0OZ3fFQMEO9dXqlmjgq1bGD-Xa8uKKQm-H-NiJeC_ag1juzzAX26XRoColMQAvD_BwE'
 
 @Component({
   selector: 'app-scroll-to-top',
@@ -10,13 +10,12 @@ const MY_NEWS_LINK = "https://help.rescue.org/donate/ukraine-acq?ms=gs_ppc_fy22_
 })
 export class ScrollToTopComponent implements OnInit {
 
-  @Input('inputWindow') inputWindow: ElementRef
+  @Input() inputWindow: ElementRef
 
   @ViewChild('scrollArrow') scrollArrow: ElementRef
   @ViewChild('ourNews') ourNews: ElementRef
-  
-  public arrowOn: boolean = false
 
+  public arrowOn: boolean = false
   public myNewsLink = MY_NEWS_LINK
 
   constructor() { }
@@ -25,48 +24,31 @@ export class ScrollToTopComponent implements OnInit {
     $('#spotbieMainSpotBieScroll').animate({ scrollTop: 0 }, 'slow')
   }
 
-  addScrollEvent() {
-
-  }
-
   goToNews() {
-    window.open(this.myNewsLink, "_blank")
+    window.open(this.myNewsLink, '_blank')
   }
 
-  ngOnInit(): void {
-
-    this.addScrollEvent()
-
-  }
+  ngOnInit(): void {}
 
   ngAfterViewInit(){
-
     $('#spotbieMainSpotBieScroll').on('scroll', function() {
-      // do your things like logging the Y-axis
-      
       const scrollTop = $('#spotbieMainSpotBieScroll').scrollTop()
-
-      if (scrollTop < 119) {        
+      if (scrollTop < 119) {
         this.scrollArrow.nativeElement.className = 'spotbie-scroll-top spotbie-arrow-transparent'
         this.ourNews.nativeElement.className = 'sb-your-news'
         this.arrowOn = false
-      } else if (this.arrowOn == false && scrollTop > 120) {        
+      } else if (this.arrowOn === false && scrollTop > 120) {
         this.arrowOn = true
         this.scrollArrow.nativeElement.className = 'spotbie-scroll-top'
         this.ourNews.nativeElement.className = 'sb-your-news marginLeft'
       }
-
     }.bind(this))
 
     const scrollTop = $('#spotbieMainSpotBieScroll').scrollTop()
 
     if (scrollTop > 50) {
-
       this.scrollArrow.nativeElement.className = 'spotbie-scroll-top'
       this.arrowOn = true
-
     }
-
   }
-
 }
