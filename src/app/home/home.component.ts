5d97b24babe5ab202c4145d5eec324f9c5208d1a
0456franco <franco.petitfour001@gmail.com>
2024-09-05 23:31:12 -0400

    Clicking Sign up as user now takes you to the subdomain version.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -29,7 +29,7 @@ export class HomeComponent implements OnInit {
   }
 
   signUp() {
-    this.appMenuLoggedOut.signUp();
+    window.location.assign('https://home.spotbie.com/');
   }
 
   async ngOnInit() {
